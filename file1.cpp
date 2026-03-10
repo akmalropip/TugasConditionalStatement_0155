0d10e24a63caa12470ec84007a7f091c08c60a11
akmalropip <akmalropip@gmail.com>
2026-03-10 10:12:14 +0700

    membuat fungsi2

--- file1.cpp
+++ file1.cpp
@@ -5,3 +5,18 @@ using namespace std;
 float hitungBMI(float berat, float tinggi) {
     return berat / (tinggi * tinggi);
 }
+
+//membuat fungsi 2 untuk mengecek kondisi berat badan
+string tentukanStatus(float bmi) {
+    if (bmi < 18.5) {
+        return "Berat Badan Kurang";
+    } else 
+    if (bmi < 25) {
+        return "Berat Badan Normal";
+    } else 
+    if (bmi < 30) {
+        return "Berat Badan Kelebihan";
+    } else {
+        return "Obesitas";
+    }
+}
